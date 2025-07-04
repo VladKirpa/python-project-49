eccebca058b1a0abe9e21b8bc755f3c78f02d81b
Vladislav Kirpa <vladkiirpa@gmail.com>
2025-07-04 22:28:15 +0300

    linter fix

--- brain_games/scripts/brain_prime.py
+++ brain_games/scripts/brain_prime.py
@@ -1,11 +1,8 @@
-from brain_games.games import prime
 from brain_games.scripts.brain_engine import run_game
-
+from brain_games.games import prime
 
 def main():
     run_game(prime)
 
-
 if __name__ == '__main__':
     main()
-
