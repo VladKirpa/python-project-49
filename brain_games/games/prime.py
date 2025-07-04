815cbf553dbe42fb29e222ab77826e2c73a3d354
Vladislav Kirpa <vladkiirpa@gmail.com>
2025-07-04 21:07:19 +0300

    add new game

---
+++ brain_games/games/prime.py
@@ -0,0 +1,22 @@
+import random
+
+RULES = 'Answer "yes" if given number is prime. Otherwise answer "no".'
+
+
+def is_prime(x):
+    if x == 1:
+        return False
+
+    for i in range(2, int(x ** 0.5) + 1):
+        if x % i == 0:
+            return False
+    return True
+
+
+def get_question_answer():
+
+    question = random.randint(2, 70)
+    correct_answer = 'yes' if is_prime(question) == True else 'no'
+    
+    return question, str(correct_answer)
+
---
+++ brain_games/scripts/brain_prime.py
@@ -0,0 +1,11 @@
+from brain_games.games import prime
+from brain_games.scripts.brain_engine import run_game
+
+
+def main():
+    run_game(prime)
+
+
+if __name__ == '__main__':
+    main()
+
